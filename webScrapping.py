b0ad195086d28b6ee00c2d9b4a3fc83961bb1212
Juan Cortez Zamar <106788005+juancorzamar93@users.noreply.github.com>
2023-12-18 12:52:50 -0300

    primera version

---
+++ webScrapping.py
@@ -0,0 +1,15 @@
+#Primera version
+import urllib.request
+
+term = "artificial intelligence"
+page_number = 1
+per_page = 20
+
+u = urllib.request.urlopen('https://www.ted.com/search?page={}&per_page={}&q={}'.format(page_number, per_page, term.replace(" ", "+") ))
+data = u.read()
+
+from lxml import html
+doc = html.document_formatstring(data)
+
+for title in doc.cssselect("article.search_result h3 a"):
+    print(title.text_content())
